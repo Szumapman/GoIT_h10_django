41e294c7a3b9f6ab49a8373d23b99d35d654e877
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-07 21:02:26 +0100

    Create urls.py

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+"""URLs patterns for users app."""
+
+from django.urls import path
+
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("login/", views.loginuser, name="login"),
+    path("logout/", views.logoutuser, name="logout"),
+]
