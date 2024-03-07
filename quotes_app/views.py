33d73faec609c3693b6284379acaef116dcd006c
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-07 17:51:37 +0100

    Add index and author

--- quotes_app/views.py
+++ quotes_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Author, Quote, Tag
+
 # Create your views here.
+def index(request):
+    quotes = Quote.objects.all()
+    context = {"quotes": quotes}
+    return render(request, "quotes_app/index.html", context)
+
+def author(request, author_id):
+    author = Author.objects.get(id=author_id)
+    context = {"author": author}
+    return render(request, "quotes_app/about.html", context)
