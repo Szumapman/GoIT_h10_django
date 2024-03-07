a6b3efdb6dae9886e906b00d35175cbf5ee227df
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-07 19:59:17 +0100

    add path to users

--- goit_h10_django/urls.py
+++ goit_h10_django/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("quotes_app.urls")),
+    path("users/", include("users.urls")),
 ]
