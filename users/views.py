c11cae4b6aa58aebeda4a891ae27011366bb8e7b
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-30 16:50:27 +0100

    Add profile

--- users/views.py
+++ users/views.py
@@ -6,10 +6,11 @@ from django.urls import reverse
 
 from .forms import RegisterForm, LoginForm
 
+
 def register(request):
     if request.user.is_authenticated:
         return redirect("quotes_app:index")
-    
+
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -17,27 +18,34 @@ def register(request):
             return redirect("quotes_app:index")
         else:
             return render(request, "users/register.html", {"form": form})
-        
+
     return render(request, "users/register.html", {"form": RegisterForm()})
 
 
 def loginuser(request):
     if request.user.is_authenticated:
         return redirect("quotes_app:index")
-    
+
     if request.method == "POST":
-        user = authenticate(username=request.POST["username"], password=request.POST["password"])
+        user = authenticate(
+            username=request.POST["username"], password=request.POST["password"]
+        )
         if user is None:
             messages.error(request, "Username or password is incorrect")
             return redirect(to="users:login")
-        
+
         login(request, user)
         return redirect(reverse("quotes_app:index"))
-    
+
     return render(request, "users/login.html", context={"form": LoginForm()})
-            
-            
+
+
 @login_required
 def logoutuser(request):
     logout(request)
     return redirect(reverse("quotes_app:index"))
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
