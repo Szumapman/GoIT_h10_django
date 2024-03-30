ceb41594edb9d91c5371947d9e77d905fa0fc8e7
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-30 16:55:32 +0100

    Delete profile path

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.loginuser, name="login"),
     path("logout/", views.logoutuser, name="logout"),
-    path("profile/", views.profile, name="profile"),
 ]
