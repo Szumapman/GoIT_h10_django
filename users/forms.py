134bedbb983f4ddc306cbb2005ffc506aa597e8a
Paweł Szumański <pawel.szumanski@tuta.io>
2024-03-30 16:29:03 +0100

    Add email to RegisterForm + reformat

--- users/forms.py
+++ users/forms.py
@@ -2,24 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
-    username = forms.CharField(max_length=100, 
-                                required=True, 
-                                widget=forms.TextInput())
-    password1 = forms.CharField(min_length=8,
-                                max_length=80,
-                                required=True,
-                                widget=forms.PasswordInput())
-    password2 = forms.CharField(min_length=8,
-                                max_length=80,
-                                required=True,
-                                widget=forms.PasswordInput())
-    
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput())
+    email = forms.EmailField(required=True, widget=forms.EmailInput())
+    password1 = forms.CharField(
+        min_length=8, max_length=80, required=True, widget=forms.PasswordInput()
+    )
+    password2 = forms.CharField(
+        min_length=8, max_length=80, required=True, widget=forms.PasswordInput()
+    )
+
     class Meta:
         model = User
-        fields = ("username", "password1", "password2")
-        
-        
+        fields = ("username", "email", "password1", "password2")
+
+
 class LoginForm(AuthenticationForm):
     class Meta:
         model = User
